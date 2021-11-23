2faa33d995760397c2085a1746bd5599b1125cdb
KiemNX <kiemnguyenxuan@gmail.com>
2021-11-23 21:19:47 +0700

    Init project

---
+++ src/lesson1/Lesson1.java
@@ -0,0 +1,23 @@
+package lesson1;
+
+
+import java.util.Scanner;
+
+public class Lesson1 {
+
+    private int instanceVariable = 30;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao so nguyen a:");
+        int a = scanner.nextInt();
+        System.out.println("So ban vua nhap la: " + a);
+
+    }
+
+    private int sum(int a, int b){
+        int sum = a + b;
+        sum = sum + instanceVariable;
+        return sum;
+    }
+}
